39ed7f80d50e3482e052a1808c679439405bc503
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-06-27 11:35:18 +0200

    scripting

--- fetch.py
+++ fetch.py
@@ -46,7 +46,7 @@ for r in resources:
     open('/home/jovyan/work/repo-git/'+folder_name+'/'+r['resource_title'], 'wb').write(response.content)
     f.write('\n\n### Jeu de données ['+r['dataset_title']+'](https://www.data.gouv.fr/fr/datasets/'+r['dataset_id']+')')
     f.write('\n\nResource '+r['resource_title'])
-    f.write('\n\n```python\nobj = s3.get_object(Bucket="geoffrey", Key="'+folder_name+'"/'+r['resource_title']+'")\ndf'+str(cpt)+' = pd.read_csv(obj["Body"], sep=None)\n```')
+    f.write('\n\n```python\nobj = s3.get_object(Bucket="geoffrey", Key="'+folder_name+'/'+r['resource_title']+'")\ndf'+str(cpt)+' = pd.read_csv(obj["Body"])\n```')
 
 f.write('\n\nA vous de jouer')
 
